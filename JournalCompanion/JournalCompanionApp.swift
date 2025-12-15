cf44123d8c38d52928ea3155acb01670d32c12f1
Craig Eley <craigeley@gmail.com>
2025-12-15 09:26:21 -0600

    Initial Commit

---
+++ JournalCompanion/JournalCompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  JournalCompanionApp.swift
+//  JournalCompanion
+//
+//  Created by Craig Eley on 12/15/25.
+//
+
+import SwiftUI
+
+@main
+struct JournalCompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JournalCompanionTests/JournalCompanionTests.swift
@@ -0,0 +1,17 @@
+//
+//  JournalCompanionTests.swift
+//  JournalCompanionTests
+//
+//  Created by Craig Eley on 12/15/25.
+//
+
+import Testing
+@testable import JournalCompanion
+
+struct JournalCompanionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
